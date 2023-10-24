8c9879245680dacd1fb263efc6e9392452e21cca
Stijn Kos <stijnkos123@gmail.com>
2023-10-24 20:48:33 +0200

    Revert "bot.py is finished for trading with only one stock. if more is loaded it will only consider the first stock"
    This reverts commit 14e7ab5b18e2f48844a6f46b59a17efae8af2c5b.

--- Part_2/main.py
+++ Part_2/main.py
@@ -1,7 +1,5 @@
 from trainer import *
-import pandas as pd
 
-"""
 bot_amount = 1
 stock_amount = 1
 start_cash = 100000
@@ -12,18 +10,3 @@ interval = 0
 trainer = Trainer(bot_amount, stock_amount, start_cash, start_date, end_date, interval)
 
 trainer.simulate()
-"""
-
-from bot import *
-
-bot = BotMovingAverage(100000, 1, 2)
-
-data = {'A': [1, 2, 4, 5, 6]}
-df = pd.DataFrame(data)
-
-lst = ["A"]
-
-bot.initiate(lst)
-bot.calc_worth(df)
-
-print(bot.value)
